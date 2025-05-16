7931347c029189c0d0c8624a8b6e710cd6871af5
Evair Daniel <evair.vieira@gmail.com>
2025-05-15 21:19:33 -0300

    README

--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from '../user/user.module';
 import { AuthModule } from '../auth/auth.module';
-import { ProductModule } from '../product/product.module';
 import { OrderModule } from '../order/order.module';
+import { ProductModule } from '../product/product.module';
 import { ProfileModule } from '../profile/profile.module';
-
+import { UserModule } from '../user/user.module';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://root:root123@localhost:27017/'),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
     AuthModule,
     ProfileModule,
     UserModule,
@@ -18,4 +25,5 @@ import { ProfileModule } from '../profile/profile.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+// eslint-disable-next-line prettier/prettier
+export class AppModule { }
